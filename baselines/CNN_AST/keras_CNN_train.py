6cb7897ff5da8d30e03adc49b3be361feb50f6b8
leeyanghaha <416921444@qq.com>
2019-01-10 17:51:31 +0800

    add capsule model

--- baselines/CNN_AST/keras_CNN_train.py
+++ baselines/CNN_AST/keras_CNN_train.py
@@ -20,31 +20,31 @@ reviews  = ReviewLoader(ku.Movie, product_num=100).get_data()
 
 
 
-# users = userhelper.get_users(reviews)
-# user2idx = userhelper.user2idx(users)
-# ngram2idx = voca.character_n_gram_table(reviews, min_threshold=ngram_min_threshold)
-#
-# data_params = {'max_ngram_len': max_ngram_len, 'user2idx': user2idx, 'ngram2idx': ngram2idx}
-# feature_loader = FeatureLoader(**data_params)
-#
-#
-# param = {'kernel_size': [3, 5, 7], 'batch_size': 64, 'epochs': 100, 'loss': 'categorical_crossentropy',
-#  'embedding_dim': 300, 'user_num': len(user2idx), 'max_ngram_len': max_ngram_len,  'feature_num':300 ,
-#          'vocab_size': len(ngram2idx)}
-# #
-# #
-# x, y = feature_loader.load_n_gram_idx_feature_label(reviews)
+users = userhelper.get_users(reviews)
+user2idx = userhelper.user2idx(users)
+ngram2idx = voca.character_n_gram_table(reviews, min_threshold=ngram_min_threshold)
+
+data_params = {'max_ngram_len': max_ngram_len, 'user2idx': user2idx, 'ngram2idx': ngram2idx}
+feature_loader = FeatureLoader(**data_params)
+
+
+param = {'kernel_size': [3, 5, 7], 'batch_size': 64, 'epochs': 100, 'loss': 'categorical_crossentropy',
+ 'embedding_dim': 300, 'user_num': len(user2idx), 'max_ngram_len': max_ngram_len,  'feature_num':300 ,
+         'vocab_size': len(ngram2idx)}
 #
 #
-# training_split = int(0.8 * x.shape[0])
-# training_x, training_y = x[:training_split, :], y[:training_split]
-# testing_x, testing_y = x[training_split:, ], y[training_split:]
-#
-# model = TextCNN(**param)
-# model.fit(training_x, training_y)
-# model.save_weight(ku.CNN_AST_model)
-# model.load_weight(ku.CNN_AST_model)
-# res = model.evaluate(testing_x, testing_y)
-# testing_loss = res[0]
-# testing_acc = res[1]
-# print('testing_loss: {}, testing_acc: {}'.format(testing_loss, testing_acc))
+x, y = feature_loader.load_n_gram_idx_feature_label(reviews)
+
+
+training_split = int(0.8 * x.shape[0])
+training_x, training_y = x[:training_split, :], y[:training_split]
+testing_x, testing_y = x[training_split:, ], y[training_split:]
+
+model = TextCNN(**param)
+model.fit(training_x, training_y)
+model.save_weight(ku.CNN_AST_model)
+model.load_weight(ku.CNN_AST_model)
+res = model.evaluate(testing_x, testing_y)
+testing_loss = res[0]
+testing_acc = res[1]
+print('testing_loss: {}, testing_acc: {}'.format(testing_loss, testing_acc))
